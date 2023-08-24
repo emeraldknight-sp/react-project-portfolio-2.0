2456405ed461f695b213ca2df2e12bf11da32bc5
David <contato.davidalmeida@outlook.com>
2023-08-24 12:56:38 -0300

    ✅ Update: Navbar component

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -45,6 +45,8 @@ export const Navbar = () => {
         <a
           href="https://api.whatsapp.com/send?phone=5586988641961&text=Ol%C3%A1%20David%2C%20tudo%20bem%3F%20"
           className="slide-menu__button-link"
+          target="_blank"
+          rel="noopener noreferrer"
         >
           <span>Falar no WhatsApp</span>
         </a>
